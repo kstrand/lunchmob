261433f0aafca6c190a98300fcfda8b7edf4e611
Keith <keith.strand@gmail.com>
2012-11-26 08:45:28 -0800

    chnage BASE_URL in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Fuckthatshit::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  BASE_URL = "http://lunchmob.heroku.com"
+  BASE_URL = "http://lunchmob.co"
+  
 
 end
