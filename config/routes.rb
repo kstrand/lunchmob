8a90df8996d63d73a98ddafd3fd253477a9e26e5
John Manoogian III <jm3@jm3.net>
2012-11-27 23:13:41 -0800

    rm final Fuckthatshit

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
-Fuckthatshit::Application.routes.draw do
-  
- 
+Lunchmob::Application.routes.draw do
+  # See how all your routes lay out with "rake routes"
 
   match "/mobs/:mob_id" => "mobs#show"
-  
   match "/mobs/:mob_id(/users/:user_id)" => "mobs#show"
 
   resources :mobs
-  
   resources :users
 
   root to: 'users#new'
-  
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
+
