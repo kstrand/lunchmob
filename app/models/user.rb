08322cf3a11fd976e874083e9304e91cb061212e
Keith <keith.strand@gmail.com>
2012-11-27 22:57:03 -0800

    fix phone number bug with rexep validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ require 'twilio-ruby'
 class User < ActiveRecord::Base
   attr_accessible :name, :phone_number, :email
   validates_uniqueness_of :name, :scope => :phone_number
+  validates :phone_number, :format => { :with => /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, :message => "not a good number" }
   validates_presence_of :name
+  
 
   belongs_to :mob
 
