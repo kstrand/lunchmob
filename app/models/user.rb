5c9c73f269f6a3bf04690e62bbbd39f5163055d3
Keith <keith.strand@gmail.com>
2012-11-28 19:23:17 -0800

    regexp to prevent invalid phone number entry

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ require 'twilio-ruby'
 class User < ActiveRecord::Base
   attr_accessible :name, :phone_number, :email
   validates_uniqueness_of :name, :scope => :phone_number
-  validates :phone_number, :format => { :with => /^\(?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$/, :message => "not a good number" }
+  validates :phone_number, :format => { :with => /^\(?([2-9][0-8][0-9])\)?[-]([2-9][0-9]{2})[-]([0-9]{4})$/, :message => "please use dashes, thanks" }
   validates_presence_of :name
-  
-
   belongs_to :mob
 
 end
