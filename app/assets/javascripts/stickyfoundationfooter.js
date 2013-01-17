bea5f7da747c0c0f117faeb0aff31e6e1881a187
Keith <keith.strand@gmail.com>
2013-01-16 16:16:57 -0800

    take out sticky footer

---
+++ app/assets/javascripts/stickyfoundationfooter.js
@@ -0,0 +1,8 @@
+stickyfoundationfooter.js
+
+$(".footer, .push").height($(".footer .row").height()+"px");
+$(".wrapper").css({'margin-bottom':(-1*$(".footer .row").height())+"px"});
+window.onresize = function(){
+    $(".footer, .push").height($(".footer .row").height()+"px");
+    $(".wrapper").css({'margin-bottom':(-1*$(".footer .row").height())+"px"});
+}
